0da9f1c5730369837468e05875afbfb9e5536091
ANDERSON IGLIKOSKI <anderson_iglikoski@estudante.sesisenai.org.br>
2025-04-07 19:24:13 -0300

    Primeiro commit

--- src/app/utilities/zodvalidation.tsx
+++ src/app/utilities/zodvalidation.tsx
@@ -1,7 +1,16 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 export const zodValidation = z.object({
- nome: z.string()
- .min(1, { message: 'Nome é obrigatório' })
- .min(3, { message: 'Nome deve ter pelo menos 3 caracteres' })
+    nome: z.string()
+        .min(1, { message: 'Nome é obrigatório' })
+        .min(3, { message: 'Nome deve ter no máximo 3 caracteres' }),
+    email: z.string()
+        .min(1, { message: 'O email é obrigatório' })
+        .email({ message: 'Email invalido' })
+        .min(8, { message: 'O email deve ter no máximo 8 caracteres' }),
+    cpf: z.string()
+        .min(1, { message: 'O CPF é obrigatório' })
+        .length(14, { message: 'O CPF deve ter 11 caracteres' }),
+    telefone: z.string()
+        .optional()
 })
